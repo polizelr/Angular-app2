a62d8d9a222e26a4f47c134d0c442881f09ad165
Rafaela <polizelc.rafaela@gmail.com>
2019-02-25 14:58:32 -0300

    adding a service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //cria a variável como sendo um atributo do objeto
+  constructor(private ofertasServices: OfertasService) { }
+
 
   ngOnInit() {
+    console.log(this.ofertasServices.getOfertas())
+
   }
 
 }
